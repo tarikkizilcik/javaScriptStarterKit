6947fe03e59d2ffb9afaa81e61814cbf6f90ff4b
fatih.cakmak@msuyazilim.com <fatih.cakmak@msuyazilim.com>
2021-10-19 00:10:20 +0300

    JS Bootcamp - Lesson 1 - Oct 19, 2021

---
+++ src/app.js
@@ -0,0 +1,9 @@
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
+
+konutKredileri.forEach(element => {
+    console.log(element);
+});
+
+for(let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+}
